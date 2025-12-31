195290ff1be5b9ad4672bc05ed1f724b0d5d28e6
vinicius42994 <geraldovenicius41@gmail.com>
2025-12-30 21:47:58 -0300

    Primeiro commit

---
+++ 25.py
@@ -0,0 +1,31 @@
+while True: 
+    print("-----Menu-----")
+    print("1 - Somar")
+    print("2 - Subtrair")
+    print("3 - Multiplicar")
+    print("4 - Dividir")
+    print("5 - Sair")
+    print("----------")
+    escolha = int(input("Escolha uma operação ou tecle 5 para sair: "))
+    print("----------")
+    if escolha >= 1 and escolha <=4:
+        a = float(input("Digite o primeiro numero: "))
+        print("----------")
+        b = float(input("Digite o segundo numero: "))
+        print("----------")
+        if escolha == 1:
+            print(f"Resultado: {a+b}")
+        elif escolha == 2:
+            print(f"Resultado: {a-b}")
+        elif escolha == 3:
+            print(f"Resultado: {a*b}")
+        elif escolha == 4:
+            if b != 0:
+                print(f"Resultado: {a/b}")
+            else:
+                print("Erro: Divisão por zero!")    
+    elif escolha == 5:
+        print("Saindo do programa")        
+        break
+    else:
+        print("Opção inválida")
